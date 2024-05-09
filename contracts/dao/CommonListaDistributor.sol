79960ea40dcb42a11339266c6dd790de2ee301e0
rick <jing.xin@lista.org>
2024-05-09 16:50:00 +0800

    fea: add Lista distributor

---
+++ contracts/dao/CommonListaDistributor.sol
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "./interfaces/IVault.sol";
+
+/**
+  * @title CommonListaDistributor
+  * @dev lista token stake and distributor
+ */
+abstract contract CommonListaDistributor is Initializable, AccessControlUpgradeable {
+    using SafeERC20 for IERC20;
+
+    event LPTokenDeposited(address indexed lpToken, address indexed receiver, uint256 amount);
+    event LPTokenWithdrawn(address indexed lpToken, address indexed receiver, uint256 amount);
+    event RewardClaimed(address indexed receiver, uint256 listaAmount);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event RecoveredERC20(address token, uint256 amount);
+
+    // LP token address
+    address public lpToken;
+    // lista Vault address
+    IVault public vault;
+    // token name
+    string public name;
+    // token symbol
+    string public symbol;
+    // total supply
+    uint256 public totalSupply;
+    // balance of each account
+    // account -> balance
+    mapping(address => uint256) public balanceOf;
+    // period finish
+    uint256 public periodFinish;
+    // last update operation timestamp
+    uint256 public lastUpdate;
+    // reward of per token
+    uint256 public rewardIntegral;
+    // reward of per second
+    uint256 public rewardRate;
+    // reward integral for each account on last update time
+    // account -> reward integral
+    mapping(address => uint256) public rewardIntegralFor;
+    // pending reward for each account
+    // account -> pending reward
+    mapping(address => uint256) private storedPendingReward;
+    // distributor id
+    uint16 public distributorId;
+    // manager role
+    bytes32 public constant MANAGER = keccak256("MANAGER");
+    // vault role
+    bytes32 public constant VAULT = keccak256("VAULT");
+    // reward duration is 1 week
+    uint256 constant REWARD_DURATION = 1 weeks;
+    // decimals
+    uint8 public constant decimals = 18;
+
+    // deposit token and get rewards
+    function _deposit(address _account, uint256 amount) internal {
+        require(amount > 0, "cannot deposit zero");
+        uint256 balance = balanceOf[_account];
+        uint256 supply = totalSupply;
+
+        balanceOf[_account] = balance + amount;
+        totalSupply = supply + amount;
+
+        _updateReward(_account, balance, supply);
+
+        if (getWeek(block.timestamp) >= getWeek(periodFinish)) _fetchRewards();
+
+        emit Transfer(address(0), _account, amount);
+        emit LPTokenDeposited(address(lpToken), _account, amount);
+    }
+
+    // withdraw token
+    function _withdraw(address _account, uint256 amount) internal {
+        uint256 balance = balanceOf[_account];
+        uint256 supply = totalSupply;
+        require(balance >= amount, "insufficient balance");
+        balanceOf[_account] = balance - amount;
+        totalSupply = supply - amount;
+
+        _updateReward(_account, balance, supply);
+        if (getWeek(block.timestamp) >= getWeek(periodFinish)) _fetchRewards();
+
+        emit Transfer(_account, address(0), amount);
+        emit LPTokenWithdrawn(address(lpToken), _account, amount);
+    }
+
+    // when account do write operation, update reward
+    function _updateReward(address _account, uint256 balance, uint256 supply) internal {
+        // update reward
+        uint256 updated = periodFinish;
+        if (updated > block.timestamp) updated = block.timestamp;
+        uint256 duration = updated - lastUpdate;
+        if (duration > 0) lastUpdate = uint32(updated);
+
+        if (duration > 0 && supply > 0) {
+            rewardIntegral += (duration * rewardRate * 1e18) / supply;
+        }
+        if (_account != address(0)) {
+            uint256 integralFor = rewardIntegralFor[_account];
+            if (rewardIntegral > integralFor) {
+
+                storedPendingReward[_account] += uint128((balance * (rewardIntegral - integralFor)) / 1e18);
+                rewardIntegralFor[_account] = rewardIntegral;
+            }
+        }
+    }
+
+    /**
+      * @dev get claimable reward amount
+      * @param account account address
+      * @return reward amount
+      */
+    function claimableReward(
+        address account
+    ) external view returns (uint256) {
+        uint256 updated = periodFinish;
+        if (updated > block.timestamp) updated = block.timestamp;
+        uint256 duration = updated - lastUpdate;
+        uint256 balance = balanceOf[account];
+        uint256 supply = totalSupply;
+
+        uint256 integral = rewardIntegral;
+        if (supply > 0) {
+            integral += (duration * rewardRate * 1e18) / supply;
+        }
+        uint256 integralFor = rewardIntegralFor[account];
+        return storedPendingReward[account] + ((balance * (integral - integralFor)) / 1e18);
+    }
+
+    /**
+      * @dev claim reward, only vault can call this function
+      * @param _account account address
+      * @return reward amount
+      */
+    function vaultClaimReward(address _account) onlyRole(VAULT) external returns (uint256) {
+        return _claimReward(_account);
+    }
+
+    function claimReward() external returns (uint256) {
+        address _account = msg.sender;
+        uint256 amount = _claimReward(_account);
+        require(amount > 0, "no reward to claim");
+        vault.transferAllocatedTokens(distributorId, _account, amount);
+        return amount;
+    }
+
+    function _claimReward(address _account) internal returns (uint256) {
+        _updateReward(_account, balanceOf[_account], totalSupply);
+        uint256 amount = storedPendingReward[_account];
+        delete storedPendingReward[_account];
+
+        emit RewardClaimed(_account, amount);
+        return amount;
+    }
+
+    /**
+      * @dev fetch rewards weekly
+      */
+    function fetchRewards() external {
+        require(getWeek(block.timestamp) >= getWeek(periodFinish), "Can only fetch once per week");
+        _updateReward(address(0), 0, totalSupply);
+        _fetchRewards();
+    }
+
+    // fetch rewards
+    function _fetchRewards() internal {
+        uint256 amount;
+        uint16 id = distributorId;
+        if (id > 0) {
+            amount = vault.allocateNewEmissions(id);
+        }
+
+        uint256 _periodFinish = periodFinish;
+        if (block.timestamp < _periodFinish) {
+            uint256 remaining = _periodFinish - block.timestamp;
+            amount += remaining * rewardRate;
+        }
+
+        rewardRate = amount / REWARD_DURATION;
+
+        lastUpdate = block.timestamp;
+        periodFinish = block.timestamp + REWARD_DURATION;
+    }
+
+    /**
+      * @dev notify registered emission id, only vault can call this function
+      * @param _distributorId emission id
+      * @return success
+      */
+    function notifyRegisteredId(uint16 _distributorId) onlyRole(VAULT) external returns (bool) {
+        require(distributorId == 0, "Already registered");
+        require(_distributorId > 0, "Invalid emission id");
+        distributorId = _distributorId;
+        return true;
+    }
+
+    /**
+      * @dev get week number by timestamp
+      * @param timestamp timestamp
+      * @return week number
+      */
+    function getWeek(uint256 timestamp) public view returns (uint16) {
+        return vault.getWeek(timestamp);
+    }
+
+    /**
+      * @dev recover ERC20 token added to support recovering LP Rewards and other mistaken tokens from other systems to be distributed to holders
+      * @param token ERC20 token address
+      * @param amount token amount
+      */
+    function recoverERC20(address token, uint256 amount) external onlyRole(MANAGER) {
+        // Only the owner address can ever receive the recovery withdrawal
+        IERC20(token).safeTransfer(msg.sender, amount);
+        emit RecoveredERC20(token, amount);
+    }
+}
---
+++ test/ERC20LpListaDistributor.t.sol
@@ -0,0 +1,213 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "../contracts/VeLista.sol";
+import "../contracts/ListaToken.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "../contracts/dao/ERC20LpListaDistributor.sol";
+import "../contracts/dao/ListaVault.sol";
+import "../contracts/mock/MockERC20.sol";
+import "../contracts/dao/interfaces/IDistributor.sol";
+
+contract ERC20LpListaDistributorTest is Test {
+    VeLista public veLista = VeLista(0x51075B00313292db08f3450f91fCA53Db6Bd0D11);
+    ListaToken public lista = ListaToken(0x1d6d362f3b2034D9da97F0d1BE9Ff831B7CC71EB);
+    ProxyAdmin public proxyAdmin = ProxyAdmin(0xc78f64Cd367bD7d2922088669463FCEE33f50b7c);
+    uint256 MAX_UINT = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+    ListaVault listaVault;
+    MockERC20 lpToken;
+    ERC20LpListaDistributor erc20Distributor;
+
+    address manager = 0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232;
+    address user1 = 0x5a97ba0b0B18a618966303371374EBad4960B7D9;
+    address user2 = 0x245b3Ee7fCC57AcAe8c208A563F54d630B5C4eD7;
+
+    address proxyAdminOwner = 0x6616EF47F4d997137a04C2AD7FF8e5c228dA4f06;
+
+    function setUp() public {
+        vm.deal(user1, 100 ether);
+        vm.deal(user2, 100 ether);
+
+        vm.startPrank(manager);
+        lpToken = new MockERC20(manager, "LisUSD-BNB lp", "LisUSD-BNB lp");
+        lpToken.mint(manager, 1_000_000_000 ether);
+        vm.stopPrank();
+
+        vm.startPrank(proxyAdminOwner);
+        ListaVault listaVaultLogic = new ListaVault();
+        TransparentUpgradeableProxy listaVaultProxy = new TransparentUpgradeableProxy(
+            address(listaVaultLogic),
+            proxyAdminOwner,
+            abi.encodeWithSignature("initialize(address,address,address,address)", manager, manager, address(lista), address(veLista))
+        );
+        listaVault = ListaVault(address(listaVaultProxy));
+
+        ERC20LpListaDistributor distributorLogic = new ERC20LpListaDistributor();
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+            address(distributorLogic),
+            proxyAdminOwner,
+            abi.encodeWithSignature("initialize(address,address,address,address)", manager, manager, address(listaVault), address(lpToken))
+        );
+        erc20Distributor = ERC20LpListaDistributor(address(proxy));
+        vm.stopPrank();
+
+        vm.prank(user1);
+        lpToken.approve(address(erc20Distributor), MAX_UINT);
+
+        vm.prank(user2);
+        lpToken.approve(address(erc20Distributor), MAX_UINT);
+    }
+
+    function test_depositRewards() public {
+        uint16 currentWeek = veLista.getCurrentWeek();
+        vm.startPrank(manager);
+        lista.approve(address(listaVault), MAX_UINT);
+        listaVault.depositRewards(100 ether, currentWeek+1);
+        listaVault.depositRewards(200 ether, currentWeek+2);
+        vm.stopPrank();
+
+        uint256 week1Emission = listaVault.weeklyEmissions(currentWeek+1);
+        uint256 week2Emission = listaVault.weeklyEmissions(currentWeek+2);
+        assertEq(week1Emission, 100 ether);
+        assertEq(week2Emission, 200 ether);
+    }
+
+    function test_registerReceiver() public {
+        vm.startPrank(manager);
+        uint16 id = listaVault.registerDistributor(address(erc20Distributor));
+        vm.stopPrank();
+
+        assertEq(listaVault.idToDistributor(id), address(erc20Distributor), "register receiver failed");
+        assertEq(erc20Distributor.distributorId(), id, "register receiver id error");
+    }
+
+    function test_setWeeklyReceiverPercent() public {
+        uint16 currentWeek = veLista.getCurrentWeek();
+        vm.startPrank(manager);
+        uint16 id = listaVault.registerDistributor(address(erc20Distributor));
+        uint16[] memory ids = new uint16[](1);
+        ids[0] = id;
+        uint256[] memory percents = new uint256[](1);
+        percents[0] = 1e18;
+        listaVault.setweeklyDistributorPercent(currentWeek+1, ids, percents);
+
+        lista.approve(address(listaVault), MAX_UINT);
+        listaVault.depositRewards(100 ether, currentWeek+1);
+        vm.stopPrank();
+
+        assertEq(listaVault.weeklyDistributorPercent(currentWeek+1, 0), 1, "set weekly receiver percent failed");
+        assertEq(listaVault.weeklyDistributorPercent(currentWeek+1, id),  1e18, "set weekly receiver percent failed");
+        assertEq(listaVault.getDistributorWeeklyEmissions(id, currentWeek+1), 100 ether, "get receiver weekly emissions error");
+    }
+
+    function test_deposit() public {
+        vm.startPrank(manager);
+        lpToken.transfer(user1, 10000 ether);
+        lpToken.transfer(user2, 10000 ether);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        erc20Distributor.deposit(100 ether);
+        vm.stopPrank();
+
+        uint256 balance = erc20Distributor.balanceOf(user1);
+        uint256 totalSupply = erc20Distributor.totalSupply();
+        assertEq(balance, 100 ether, "user1 balance error");
+        assertEq(totalSupply, 100 ether, "total supply error");
+    }
+
+    function test_withdraw() public {
+        vm.startPrank(manager);
+        lpToken.transfer(user1, 10000 ether);
+        lpToken.transfer(user2, 10000 ether);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        erc20Distributor.deposit(100 ether);
+        erc20Distributor.withdraw(10 ether);
+        vm.stopPrank();
+
+        uint256 balance = erc20Distributor.balanceOf(user1);
+        uint256 totalSupply = erc20Distributor.totalSupply();
+        uint256 lpBalance = lpToken.balanceOf(user1);
+        assertEq(balance, 90 ether, "user1 balance error");
+        assertEq(totalSupply, 90 ether, "total supply error");
+        assertEq(lpBalance, 10000 ether - 90 ether, "lp balance error");
+    }
+
+    function test_fetchRewards() public {
+        uint16 currentWeek = veLista.getCurrentWeek();
+        uint256 weekAmount = 700 ether;
+        vm.startPrank(manager);
+
+        lista.approve(address(listaVault), MAX_UINT);
+        listaVault.depositRewards(weekAmount, currentWeek+1);
+
+        uint16 id = listaVault.registerDistributor(address(erc20Distributor));
+
+        uint16[] memory ids = new uint16[](1);
+        ids[0] = id;
+        uint256[] memory percents = new uint256[](1);
+        percents[0] = 1e18;
+        listaVault.setweeklyDistributorPercent(currentWeek+1, ids, percents);
+
+        vm.stopPrank();
+
+        skip(1 weeks);
+
+        vm.startPrank(user1);
+        erc20Distributor.fetchRewards();
+        vm.stopPrank();
+
+        assertEq(erc20Distributor.rewardRate(), weekAmount / 1 weeks, "reward rate error");
+        assertEq(erc20Distributor.lastUpdate(), block.timestamp, "last update error");
+        assertEq(erc20Distributor.periodFinish(), block.timestamp+1 weeks, "period finish error");
+    }
+
+    function test_claimReward() public {
+        uint16 currentWeek = veLista.getCurrentWeek();
+        uint256 weekAmount = 700 ether;
+        vm.startPrank(manager);
+
+        lista.approve(address(listaVault), MAX_UINT);
+        listaVault.depositRewards(weekAmount, currentWeek+1);
+
+        uint16 id = listaVault.registerDistributor(address(erc20Distributor));
+
+        uint16[] memory ids = new uint16[](1);
+        ids[0] = id;
+        uint256[] memory percents = new uint256[](1);
+        percents[0] = 1e18;
+        listaVault.setweeklyDistributorPercent(currentWeek+1, ids, percents);
+
+        lpToken.transfer(user1, 10000 ether);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        lpToken.approve(address(erc20Distributor), MAX_UINT);
+        erc20Distributor.deposit(100 ether);
+
+        skip(1 weeks);
+        erc20Distributor.fetchRewards();
+
+        IDistributor[] memory distributors = new IDistributor[](1);
+        distributors[0] = IDistributor(address(erc20Distributor));
+
+
+        vm.stopPrank();
+
+        skip(1 days);
+        uint256 claimable = erc20Distributor.claimableReward(user1);
+        uint256 rewardRate = erc20Distributor.rewardRate();
+        assertEq(claimable, rewardRate*1 days, "claimable rewards error");
+
+        vm.startPrank(user1);
+        listaVault.batchClaimRewards(distributors);
+        vm.stopPrank();
+        uint256 listaBalance = lista.balanceOf(user1);
+        assertEq(listaBalance, claimable, "user1 lista balance error");
+    }
+
+}
