efe8a98922c059a866be72e730b0c85d30452373
Lawson Cheng <lawson.c@binance.com>
2024-06-01 00:45:55 +0800

    Updated contract comment

--- contracts/oft/PausableAlt.sol
+++ contracts/oft/PausableAlt.sol
@@ -7,30 +7,38 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // @notice An alternative version of the openzeppelin Pausable contract
 abstract contract PausableAlt is Pausable, Ownable {
 
-  // an multi-sig address to pause the transferal function in emergency
+  // a multi-sig address to pause the transferal function in emergency
   address public multiSig;
 
-  /**
-   * @dev Modifier to make a function callable only when the caller equals to the multiSig address.
-   */
+  // @dev Modifier to make a function callable only when the caller equals to the multiSig address.
   modifier isMultiSig() {
     require(multiSig != address(0), "PausableAlt: multiSig not set");
     require(msg.sender == multiSig, "PausableAlt: not multiSig");
     _;
   }
-
+  // @dev Emitted when the multiSig address is updated
   event MultiSigUpdated(address oldMultiSig, address newMultiSig);
 
+  /**
+   * @notice Set the multiSig address
+   * @param _multiSig The new multiSig address
+   */
   function setMultiSig(address _multiSig) external onlyOwner {
     address oldMultiSig = multiSig;
     multiSig = _multiSig;
     emit MultiSigUpdated(oldMultiSig, _multiSig);
   }
 
+  /**
+   * @notice Pause the contract
+   */
   function pause() external isMultiSig {
     _pause();
   }
 
+  /**
+   * @notice Unpause the contract
+   */
   function unpause() external onlyOwner {
     _unpause();
   }
