282df626e294724607e4661513c633afaca2e9d2
Lawson Cheng <lawson.c@binance.com>
2024-06-03 11:16:27 +0800

    Enhanced local test scripts

--- test/oft/local.t.sol
+++ test/oft/local.t.sol
@@ -27,14 +27,16 @@ import "forge-std/console.sol";
 // DevTools imports
 import { TestHelperOz5 } from "./tools/TestHelperOz5.sol";
 
-contract MyOFTTest is TestHelperOz5, TransferLimiter {
+contract OFTTest is TestHelperOz5, TransferLimiter {
   using OptionsBuilder for bytes;
 
   uint32 aEid = 1;
   uint32 bEid = 2;
 
+  // @dev note as chain A
   ERC20Mock aToken;
   ListaOFTAdapter oftAdapter;
+  // @dev note as chain B
   ListaOFT bToken;
 
   address public userA = address(0x1);
@@ -118,7 +120,7 @@ contract MyOFTTest is TestHelperOz5, TransferLimiter {
   }
 
   // @dev Lock aToken at 1, mint bToken at 2
-  function test_send_oft() public {
+  function test_send_back_and_forth() public {
     uint256 tokensToSend = 0.1 ether;
 
     /** ---------------------------------
@@ -171,6 +173,127 @@ contract MyOFTTest is TestHelperOz5, TransferLimiter {
 
     assertEq(aToken.balanceOf(userA), initialBalance);
     assertEq(bToken.balanceOf(userB), 0);
+  }
+
+  // @dev simulate we transfer more than 100,000 tokens per day
+  function test_exceeded_daily_global_limit() public {
+    uint256 tokensToSend = 10000 ether;
+    address[11] memory users = [
+            address(0x3), address(0x4), address(0x5), address(0x6), address(0x7), address(0x8),
+            address(0x9), address(0x11), address(0x12), address(0x13), address(0x14)
+      ];
+    // transfer 10000 with 11 different user
+    // 110,000 tokens will exceed the global transfer limit
+    for (uint i = 0; i < users.length; ++i) {
+      address user = users[i];
+      vm.deal(user, 1000 ether);
+      try this.send_from_a_to_b(user, user, tokensToSend) {
+      } catch (bytes memory reason) {
+        console.log("test_exceeded_daily_global_limit(): Error caught: TransferLimitExceeded()");
+        bytes4 desiredSelector = bytes4(keccak256(bytes("TransferLimitExceeded()")));
+        bytes4 receivedSelector = bytes4(reason);
+        assertEq(desiredSelector, receivedSelector);
+      }
+    }
+    // after 1 day, the global transfer limit will be reset
+    vm.warp(vm.getBlockTimestamp() + 86401);
+    vm.deal(address(0x1234), 1000 ether);
+    send_from_a_to_b(address(0x1234), address(0x1234), 10000 ether);
+    console.log("test_exceeded_daily_global_limit(): succeeded after 1 day");
+  }
+
+  // @dev simulate an user transfer more than 20,000 tokens per day
+  function test_exceeded_daily_user_transfer_limit() public {
+    for (uint i = 0; i < 2; ++i) {
+      try this.send_from_a_to_b(userA, userA, 10000 ether) {
+      } catch (bytes memory reason) {
+        console.log("test_exceeded_daily_user_transfer_limit(): Error caught: TransferLimitExceeded()");
+        bytes4 desiredSelector = bytes4(keccak256(bytes("TransferLimitExceeded()")));
+        bytes4 receivedSelector = bytes4(reason);
+        assertEq(desiredSelector, receivedSelector);
+      }
+    }
+    // after 1 day, the user transfer limit will be reset
+    vm.warp(vm.getBlockTimestamp() + 86401);
+    send_from_a_to_b(userA, userA, 10 ether);
+    console.log("test_exceeded_daily_user_transfer_limit(): succeeded after 1 day");
+  }
+
+  // @dev simulate an user transfer more than 10 times per day
+  function test_exceeded_daily_user_transfer_attempts() public {
+    for (uint i = 0; i < 11; ++i) {
+      try this.send_from_a_to_b(userA, userA, 1 ether) {
+      } catch (bytes memory reason) {
+        console.log("test_exceeded_daily_user_transfer_attempts(): Error caught: TransferLimitExceeded()");
+        bytes4 desiredSelector = bytes4(keccak256(bytes("TransferLimitExceeded()")));
+        bytes4 receivedSelector = bytes4(reason);
+        assertEq(desiredSelector, receivedSelector);
+      }
+    }
+    // after 1 day, the user transfer attempt will be reset
+    vm.warp(vm.getBlockTimestamp() + 86401);
+    send_from_a_to_b(userA, userA, 10 ether);
+    console.log("test_exceeded_daily_user_transfer_attempts(): succeeded after 1 day");
+  }
+
+  // @dev simulate an user transfer token with an outbound amount that is too small or too large
+  function test_outbound_upper_and_lower_limit() public {
+    // amount too small
+    try this.send_from_a_to_b(userA, userA, 0.001 ether) {
+    } catch (bytes memory reason) {
+      console.log("test_outbound_upper_and_lower_limit(): Error caught: TransferLimitExceeded(): Amount too small");
+      bytes4 desiredSelector = bytes4(keccak256(bytes("TransferLimitExceeded()")));
+      bytes4 receivedSelector = bytes4(reason);
+      assertEq(desiredSelector, receivedSelector);
+    }
+    // amount too large
+    try this.send_from_a_to_b(userA, userA, 10001 ether) {
+    } catch (bytes memory reason) {
+      console.log("test_outbound_upper_and_lower_limit(): Error caught: TransferLimitExceeded(): amount too large");
+      bytes4 desiredSelector = bytes4(keccak256(bytes("TransferLimitExceeded()")));
+      bytes4 receivedSelector = bytes4(reason);
+      assertEq(desiredSelector, receivedSelector);
+    }
+  }
+
+  // @dev simulate an user couldn't transfer if the OFT is paused
+  function test_pausable() public {
+    // 1. not paused initially
+    assertEq(oftAdapter.paused(), false);
+    // 2. pause it without a multiSig address
+    vm.expectRevert("PausableAlt: multiSig not set");
+    oftAdapter.pause();
+    // 3. set multiSig address
+    oftAdapter.setMultiSig(address(this));
+    assertEq(oftAdapter.multiSig(), address(this));
+    // 4. non multiSig wallet try to pause
+    vm.startPrank(address(0x1234));
+    vm.expectRevert("PausableAlt: not multiSig");
+    oftAdapter.pause();
+    vm.stopPrank();
+    // 5. only owner can unpause
+    vm.startPrank(address(0x1234));
+    vm.expectRevert("Ownable: caller is not the owner");
+    oftAdapter.unpause();
+    vm.stopPrank();
+    // 6. pause it
+    oftAdapter.pause();
+    assertEq(oftAdapter.paused(), true);
+    // 7. transfer revert when paused
+    vm.startPrank(userA);
+    bytes memory opts = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200000, 0);
+    SendParam memory sendParam = SendParam(bEid, addressToBytes32(userA), 1 ether, 1 ether, opts, "", "");
+    MessagingFee memory fee = oftAdapter.quoteSend(sendParam, false);
+    aToken.approve(address(oftAdapter), 1 ether);
+    vm.expectRevert("Pausable: paused"); // expect "revert: Pausable: paused"
+    oftAdapter.send{ value: fee.nativeFee }(sendParam, fee, payable(address(this)));
+    vm.stopPrank();
+    console.log("test_pausable(): transfer revert when paused");
+    // 8. transfer success after unpause()
+    oftAdapter.unpause();
+    assertEq(oftAdapter.paused(), false);
+    send_from_a_to_b(userA, userA, 100 ether);
+    console.log("test_pausable(): transfer success after unpause()");
   }
 
   // Verify msg integrity
@@ -230,6 +353,27 @@ contract MyOFTTest is TestHelperOz5, TransferLimiter {
     assertEq(composer.message(), composerMsg_);
     assertEq(composer.executor(), address(this));
     assertEq(composer.extraData(), composerMsg_); // default to setting the extraData to the message as well to test
+  }
+
+  // ---- helper functions ----
+  function send_from_a_to_b(address from, address to, uint256 amt) public {
+    aToken.mint(from, amt);
+    vm.startPrank(from);
+    bytes memory opts = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200000, 0);
+    SendParam memory sendParam = SendParam(
+      bEid,
+      addressToBytes32(to),
+      amt,
+      amt,
+      opts,
+      "",
+      ""
+    );
+    MessagingFee memory fee = oftAdapter.quoteSend(sendParam, false);
+    aToken.approve(address(oftAdapter), amt);
+    oftAdapter.send{ value: fee.nativeFee }(sendParam, fee, payable(address(this)));
+    verifyPackets(bEid, addressToBytes32(address(oftAdapter)));
+    vm.stopPrank();
   }
 
 }
