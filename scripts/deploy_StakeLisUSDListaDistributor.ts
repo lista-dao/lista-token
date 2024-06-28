afedec3f230c93dc1793291208dfefd117a1a5e4
Lawson Cheng <lawson.c@binance.com>
2024-06-28 15:34:47 +0800

    Added Deployment script for Lista Distributor

---
+++ scripts/deploy_StakeLisUSDListaDistributor.ts
@@ -0,0 +1,35 @@
+import { deployProxy } from "./tasks";
+import hre from "hardhat";
+async function main() {
+  const name = "ListaDao";
+  const symbol = "Lista-Stake LisUSD ListaDAODistributor";
+  let lpToken, listaVault, admin, manager;
+  if (hre.network.name === "bsc") {
+    admin = "";
+    manager = "";
+    listaVault = "";
+    lpToken = "";
+  } else if (hre.network.name === "bscTestnet") {
+    admin = "0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232";
+    manager = "0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232";
+    listaVault = "0xCfeb269242cf988b61833910E7aaC56554F09f7b";
+    lpToken = "0x1d6d362f3b2034D9da97F0d1BE9Ff831B7CC71EB";
+  }
+  await deployProxy(
+    hre,
+    "StakeLisUSDListaDistributor",
+    name,
+    symbol,
+    admin,
+    manager,
+    listaVault,
+    lpToken
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
