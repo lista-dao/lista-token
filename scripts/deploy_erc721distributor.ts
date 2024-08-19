1cdae7e1f6e9f9bcb9aff04da24e7dccb34afea8
rick <jing.xin@lista.org>
2024-08-19 15:34:34 +0800

    add new lp deploy scripts

--- scripts/deploy_erc721distributor.ts
+++ scripts/deploy_erc721distributor.ts
@@ -1,57 +1,73 @@
 import { deployProxy } from "./tasks";
 import hre from "hardhat";
 
-const admin = "0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232";
-
 async function main() {
   let nftManager;
   if (hre.network.name === "bsc") {
     nftManager = "0x46A15B0b27311cedF172AB29E4f4766fbE7F4364";
-    // slisBNB/BNB v3 LP 0.05%
+    // LISTA/BNB v3 LP 1%
     await deployERC721Distributor(
-      "slisBNB/BNB v3 LP",
+      "LISTA/BNB v3 LP",
       nftManager,
-      "0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B",
       "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-      500,
-      "50000000000000000" // 5 * 10 ** 16
+      "0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46",
+      10000,
+      "300000000000000000" // 3 * 10 ** 17
     );
-    // lisUSD/WBNB v3 LP 0.25%
+    // LISTA/USDT v3 LP 0.25%
     await deployERC721Distributor(
-      "lisUSD/WBNB v3 LP",
+      "LISTA/USDT v3 LP",
       nftManager,
-      "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
-      "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
-      2500,
-      "100000000000000000" // 10 ** 17
-    );
-    // lisUSD/BTCB v3 LP 0.25%
-    await deployERC721Distributor(
-      "lisUSD/BTCB v3 LP",
-      nftManager,
-      "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
-      "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
-      2500,
-      "100000000000000000" // 10 ** 17
-    );
-    // lisUSD/ETH v3 LP 0.25%
-    await deployERC721Distributor(
-      "lisUSD/ETH v3 LP",
-      nftManager,
-      "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
-      "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
-      2500,
-      "100000000000000000" // 10 ** 17
-    );
-    // lisUSD/USDT v3 LP 0.05%
-    await deployERC721Distributor(
-      "lisUSD/USDT v3 LP",
-      nftManager,
-      "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
       "0x55d398326f99059fF775485246999027B3197955",
-      500,
-      "30000000000000000" // 3 * 10 ** 16
+      "0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46",
+      2500,
+      "200000000000000000" // 2 * 10 ** 17
     );
+    // // slisBNB/BNB v3 LP 0.05%
+    // await deployERC721Distributor(
+    //   "slisBNB/BNB v3 LP",
+    //   nftManager,
+    //   "0xB0b84D294e0C75A6abe60171b70edEb2EFd14A1B",
+    //   "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    //   500,
+    //   "50000000000000000" // 5 * 10 ** 16
+    // );
+    // // lisUSD/WBNB v3 LP 0.25%
+    // await deployERC721Distributor(
+    //   "lisUSD/WBNB v3 LP",
+    //   nftManager,
+    //   "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+    //   "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    //   2500,
+    //   "100000000000000000" // 10 ** 17
+    // );
+    // // lisUSD/BTCB v3 LP 0.25%
+    // await deployERC721Distributor(
+    //   "lisUSD/BTCB v3 LP",
+    //   nftManager,
+    //   "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+    //   "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+    //   2500,
+    //   "100000000000000000" // 10 ** 17
+    // );
+    // // lisUSD/ETH v3 LP 0.25%
+    // await deployERC721Distributor(
+    //   "lisUSD/ETH v3 LP",
+    //   nftManager,
+    //   "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+    //   "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    //   2500,
+    //   "100000000000000000" // 10 ** 17
+    // );
+    // // lisUSD/USDT v3 LP 0.05%
+    // await deployERC721Distributor(
+    //   "lisUSD/USDT v3 LP",
+    //   nftManager,
+    //   "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5",
+    //   "0x55d398326f99059fF775485246999027B3197955",
+    //   500,
+    //   "30000000000000000" // 3 * 10 ** 16
+    // );
   } else if (hre.network.name === "bscTestnet") {
     nftManager = "0x427bF5b37357632377eCbEC9de3626C71A5396c1";
     // slisBNB/BNB v3 LP 0.05%
