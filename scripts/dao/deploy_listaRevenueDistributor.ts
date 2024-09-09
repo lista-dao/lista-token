da3cb9f50db77ddd3aa85fe06e76ea889e3ace3b
ted-w <ted.w@lista.org>
2024-09-09 10:09:43 +0800

    ! update deploy script, integer overflow

--- scripts/dao/deploy_listaRevenueDistributor.ts
+++ scripts/dao/deploy_listaRevenueDistributor.ts
@@ -5,18 +5,29 @@ async function main() {
   const signers = await hre.ethers.getSigners();
   const deployer = signers[0].address;
   console.log("admin: ", deployer);
-  let bot, autoBuybackAddress, revenueWalletAddress;
+  let bot, listaAddress, autoBuybackAddress, revenueWalletAddress, listaDistributeToAddress;
 
   if (hre.network.name === "bsc") {
+    listaAddress = '0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46';
     bot = '';
     autoBuybackAddress = '';
     revenueWalletAddress = '';
+    listaDistributeToAddress = ''
   } else if (hre.network.name === "bscTestnet") {
+    listaAddress = '0x90b94D605E069569Adf33C0e73E26a83637c94B1';
     bot = '0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232';
     autoBuybackAddress = '';
     revenueWalletAddress = '';
+    listaDistributeToAddress = '';
+  } else if (hre.network.name === "bscLocal") {
+    listaAddress = '0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46';
+    bot = '0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232';
+    autoBuybackAddress = '0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232';
+    revenueWalletAddress = '0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232';
+    listaDistributeToAddress = '0xeA71Ec772B5dd5aF1D15E31341d6705f9CB86232';
   }
-  await deployProxy(hre, "ListaRevenueDistributor", deployer, bot, autoBuybackAddress, revenueWalletAddress, 7e17);
+
+  await deployProxy(hre, "ListaRevenueDistributor", deployer, bot, listaAddress, autoBuybackAddress, revenueWalletAddress, listaDistributeToAddress, '700000000000000000');
   console.log("deployProxy done");
 }
 
