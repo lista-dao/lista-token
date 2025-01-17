0395e2583f7d09196330da9ec970d159318297d0
rick <jing.xin@lista.org>
2025-01-17 12:40:25 +0800

    update cemBTC deploy scripts

--- scripts/dao/deploy_borrowListaDistributors.ts
+++ scripts/dao/deploy_borrowListaDistributors.ts
@@ -2,27 +2,23 @@ import { deployProxy } from "../tasks";
 import hre from "hardhat";
 
 const collateralConfigs: any[] = [{
-  symbol: 'mBTC BorrowListaDAODistributor',
-  lpToken: '0x7c1cCA5b25Fa0bC9AF9275Fb53cBA89DC172b878',
-}, {
-  symbol: 'mCake BorrowListaDAODistributor',
-  lpToken: '0x581FA684D0Ec11ccb46B1d92F1F24C8A3F95C0CA',
-}, {
-  symbol: 'mwBETH BorrowListaDAODistributor',
-  lpToken: '0x7dC91cBD6CB5A3E6A95EED713Aa6bF1d987146c8',
+  symbol: 'cemBTC BorrowListaDAODistributor',
+  lpToken: '0x4510aa2b3efd13bBFD78C9BfdE764F224ecc7f50',
 }]
 
 async function main() {
   const name = "ListaDao";
   const signers = await hre.ethers.getSigners();
   const deployer = signers[0].address;
-  let listaVault = '', manager;
+  let listaVault = '', manager, admin;
   if (hre.network.name === "bsc") {
     listaVault = "0x307d13267f360f78005f476Fa913F8848F30292A";
     manager = "0x74E17e6996f0DDAfdA9B500ab15a3AD7c2f69307"; // router address
+    admin = "0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253"
   } else if (hre.network.name === "bscTestnet") {
     listaVault = "0x1D70D733401169055002FB4450942F15C2F088d4";
     manager = "0x227eeaf69495E97c1E72A48785B8A041664b5a28"; // router address
+    admin = deployer
   }
 
   for (const collateralConfig of collateralConfigs) {
@@ -33,7 +29,7 @@ async function main() {
       "BorrowListaDistributor",
       name,
       collateralConfig.symbol,
-      deployer,
+      admin,
       manager,
       listaVault,
       collateralConfig.lpToken
