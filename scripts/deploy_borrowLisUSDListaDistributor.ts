f8ff3965f0dfdefa191bae9617efe3a94e1b4f15
rick <jing.xin@lista.org>
2025-01-17 10:49:05 +0800

    add cemBTC distributor

--- scripts/deploy_borrowLisUSDListaDistributor.ts
+++ scripts/deploy_borrowLisUSDListaDistributor.ts
@@ -2,25 +2,27 @@ import { deployProxy } from "./tasks";
 import hre from "hardhat";
 async function main() {
   const name = "ListaDao";
-  const symbol = "Lista-Borrow mwBETH ListaDAODistributor";
+  const symbol = "Lista-Borrow ceMBTC ListaDAODistributor";
   const signers = await hre.ethers.getSigners();
   const deployer = signers[0].address;
-  let lpToken, listaVault, manager;
+  let lpToken, listaVault, manager, admin;
   if (hre.network.name === "bsc") {
     listaVault = "0x307d13267f360f78005f476Fa913F8848F30292A";
-    lpToken = "0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5";
+    lpToken = ""; // cemBTC
     manager = "0xB68443Ee3e828baD1526b3e0Bdf2Dfc6b1975ec4";
+    admin = "0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253";
   } else if (hre.network.name === "bscTestnet") {
     listaVault = "0x0fD548f448AAB6dE7489F8FD1a4be1efca009f1C";
-    lpToken = "0x410E153F72Fa68D1e0A2aAF7e4be75CD0513E63E";
+    lpToken = "0x6F805B6548EaF0dC412fD4FA531183b8dD809145";
     manager = "0x227eeaf69495E97c1E72A48785B8A041664b5a28";
+    admin = deployer;
   }
   const address = await deployProxy(
     hre,
     "BorrowLisUSDListaDistributor",
     name,
     symbol,
-    deployer,
+    admin,
     manager,
     listaVault,
     lpToken
--- scripts/dev/deploy_collateralListaDistributors.ts
+++ scripts/dev/deploy_collateralListaDistributors.ts
@@ -2,28 +2,31 @@ import { deployProxy } from "../tasks";
 import hre from "hardhat";
 
 const collateralConfigs: any[] = [
-  // {
-  //   symbol: 'mBTC CollateralListaDAODistributor',
-  //   lpToken: '0x2deB430701DE0BB24ADD0c815C464e6DC6BB2634',
-  // },
+  {
+    symbol: 'cemBTC CollateralListaDAODistributor',
+    lpToken: '', // cemBTC
+  },
   // {
   //   symbol: 'mCake CollateralListaDAODistributor',
   //   lpToken: '0x70ad940d73415CDDAc47861e9691795AA7a119e1',
   // },
-  {
-  symbol: 'mwBETH CollateralListaDAODistributor',
-  lpToken: '0x410E153F72Fa68D1e0A2aAF7e4be75CD0513E63E',
-  }
+  // {
+  // symbol: 'mwBETH CollateralListaDAODistributor',
+  // lpToken: '0x410E153F72Fa68D1e0A2aAF7e4be75CD0513E63E',
+  // }
 ]
 
 async function main() {
   const name = "ListaDao";
   const signers = await hre.ethers.getSigners();
   const deployer = signers[0].address;
-  let listaVault = '', manager;
+  let listaVault = '', manager, admin;
   if (hre.network.name === "bscTestnet") {
     listaVault = "0x0fD548f448AAB6dE7489F8FD1a4be1efca009f1C";
     manager = "0x227eeaf69495E97c1E72A48785B8A041664b5a28";
+    admin = deployer
+  } else {
+    admin = "0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253";
   }
 
   for (const collateralConfig of collateralConfigs) {
@@ -34,7 +37,7 @@ async function main() {
       "CollateralListaDistributor",
       name,
       collateralConfig.symbol,
-      deployer,
+      admin,
       manager,
       listaVault,
       collateralConfig.lpToken
