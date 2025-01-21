e94932919960463e0f5e7fa0323710b0ce41c791
Razorback <raz.w@binance.com>
2025-01-21 10:45:26 +0800

    feat: update merkle root for s3 airdrop

--- scripts/deploy-airdrop.ts
+++ scripts/deploy-airdrop.ts
@@ -1,7 +1,8 @@
 import { ethers } from "hardhat";
+import hre from "hardhat";
 
-const TOKEN = "0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46";
-const ROOT = "0xe12f80c861a5ffa2756ee8fa6238c51fb5bd5114675d2cb580a08e64c0a642cf";
+let token = "";
+const ROOT = "0x93f599435d274a0e423df9717e4d19c1595513874cedcc6ab8cb8b6d7afae9d5";
 const RECLAIM_DELAY = 0;
 const START_TIME = 1737446400; // Tue Jan 21 2025 08:00:00 GMT+0000
 const END_TIME = 1755763200; // Thu Aug 21 2025 08:00:00 GMT+0000
@@ -12,13 +13,18 @@ async function main() {
   await merkleVerifier.waitForDeployment();
   console.log("MerkleVerifier deployed to:", merkleVerifier.target);
 
+  if (hre.network.name === "bsc") {
+    token = "0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46"; // LISTA
+  } else if (hre.network.name === "bscTestnet") {
+    token = "0x90b94D605E069569Adf33C0e73E26a83637c94B1";
+  }
   const ListaAirdrop = await ethers.getContractFactory("ListaAirdrop", {
     libraries: {
       MerkleVerifier: merkleVerifier.target,
     },
   });
   const listaAirdrop = await ListaAirdrop.deploy(
-    TOKEN, // token
+    token, // LISTA token
     ROOT, // root
     RECLAIM_DELAY, // reclaimDelay
     START_TIME, // startTime
@@ -31,7 +37,7 @@ async function main() {
   await run("verify:verify", {
     address: listaAirdrop.target,
     constructorArguments: [
-      TOKEN, // token
+      token, // token
       ROOT, // root
       RECLAIM_DELAY, // reclaimDelay
       START_TIME, // startTime
