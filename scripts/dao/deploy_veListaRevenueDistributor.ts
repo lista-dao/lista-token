b4424741fd697cff5f50e8693ac352fc47f2dd19
rick <jing.xin@lista.org>
2025-01-23 09:25:04 +0800

    update deploy scripts

--- scripts/dao/deploy_veListaRevenueDistributor.ts
+++ scripts/dao/deploy_veListaRevenueDistributor.ts
@@ -6,12 +6,12 @@ async function main() {
   const deployer = signers[0].address;
   let admin, manager, bot, revenueReceiver, lista, burnPercentage;
   if (hre.network.name === "bsc") {
-    admin = "";
-    manager = "";
-    bot = "";
-    revenueReceiver = "";
-    lista = "";
-    burnPercentage = "";
+    admin = "0x07D274a68393E8b8a2CCf19A2ce4Ba3518735253"; // time lock
+    manager = "0x8d388136d578dCD791D081c6042284CED6d9B0c6"; // manager
+    bot = "0x44CA74923aA2036697a3fA7463CD0BA68AB7F677"; // bot
+    revenueReceiver = "0x8d388136d578dCD791D081c6042284CED6d9B0c6"; // revenue receiver
+    lista = "0xFceB31A79F71AC9CBDCF853519c1b12D379EdC46"; // lista token
+    burnPercentage = "1000";
   } else if (hre.network.name === "bscTestnet") {
     admin = deployer;
     manager = deployer;
