9919f017fa9064928ff6268979a009bd81fc1f72
Lawson Cheng <lawson.c@binance.com>
2025-03-15 16:44:49 +0800

    Removed unused struct

--- contracts/dao/interfaces/IERC20TokenProvider.sol
+++ contracts/dao/interfaces/IERC20TokenProvider.sol
@@ -2,16 +2,6 @@
 pragma solidity ^0.8.10;
 
 interface IERC20TokenProvider {
-
-    /**
-     * Structs
-     */
-    struct Delegation {
-        address delegateTo; // who helps delegator to hold clisBNB, aka the delegatee
-        uint256 amount;
-    }
-
-
     function deposit(uint256 amount) external returns (uint256);
     function deposit(uint256 amount, address delegateTo) external returns (uint256);
     function withdraw(uint256 amount) external returns (uint256);
