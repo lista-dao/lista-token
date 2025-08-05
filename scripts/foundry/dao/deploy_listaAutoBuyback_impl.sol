8cd3567b3d83b0d125a01034f707a784a3ee4c92
rick <jing.xin@lista.org>
2025-08-05 15:25:41 +0800

    fea: add buyback and listaautobuyback deploy scripts

---
+++ scripts/foundry/dao/deploy_listaAutoBuyback_impl.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.10;
+
+import { Script, console } from "forge-std/Script.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { ListaAutoBuyback } from "../../../contracts/dao/ListaAutoBuyback.sol";
+
+contract ListaAutoBuybackDeploy is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        console.log("Deployer: ", deployer);
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy implementation
+        ListaAutoBuyback impl = new ListaAutoBuyback();
+        console.log("Implementation: ", address(impl));
+
+        vm.stopBroadcast();
+    }
+}
