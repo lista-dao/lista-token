b75e202e519abda2a6cf361201f39b6fe0cef908
rick <jing.xin@lista.org>
2025-12-11 14:20:15 +0800

    fea: buyback support liquidmesh

--- scripts/buyback/BuybackImpl.s.sol
+++ scripts/buyback/BuybackImpl.s.sol
@@ -11,38 +11,6 @@ contract BuybackImplScript is Script {
 
   function run() public {
     uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
-    address deployer = vm.addr(deployerPrivateKey);
-    console.log("Deployer: %s", deployer);
-    address admin = vm.envOr("ADMIN", deployer);
-    console.log("Admin: %s", admin);
-    address manager = vm.envOr("MANAGER", deployer);
-    console.log("Manager: %s", manager);
-    address pauser = vm.envOr("PAUSER", deployer);
-    console.log("Pauser: %s", pauser);
-    address bot = vm.envOr("BUYBACK_BOT", deployer);
-    console.log("Bot: %s", bot);
-
-    // 1Inch router
-    address oneInchRouter = vm.envAddress("BUYBACK_1INCH_ROUTER");
-    require(oneInchRouter != address(0), "1Inch router address cannot be null");
-    console.log("1Inch Router: %s", oneInchRouter);
-
-    // swap input tokens
-    address[] memory tokenIns = vm.envAddress("BUYBACK_TOKEN_INS", ",");
-    for (uint256 i = 0; i < tokenIns.length; i++) {
-      require(tokenIns[i] != address(0), "Swap input token address cannot be null");
-      console.log("Swap input token: %s, %s", i, tokenIns[i]);
-    }
-
-    // swap output token
-    address tokenOut = vm.envAddress("BUYBACK_TOKEN_OUT");
-    require(tokenOut != address(0), "Swap output token address cannot be null");
-    console.log("Swap output token: %s", oneInchRouter);
-
-    // receiver address
-    address receiver = vm.envAddress("BUYBACK_RECEIVER");
-    require(receiver != address(0), "Receiver address cannot be null");
-    console.log("Receiver: %s", receiver);
 
     vm.startBroadcast(deployerPrivateKey);
     Buyback buyback = new Buyback();
